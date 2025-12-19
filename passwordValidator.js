368b626179c3fcfbc7667008dcf812fda3d7c23e
saidoubari1 <oubarisaid30@gmail.com>
2025-12-19 11:55:28 +0100

    Ajout de la règle de l'éxistence d'au moins un chiffre dans le password

--- passwordValidator.js
+++ passwordValidator.js
@@ -1,5 +1,18 @@
 function validatePassword(password) {
-    return password.length >= 8;
+    const hasMinLength = password.length >= 8;
+    
+    let hasDigit = false;
+    for (let i = 0; i < password.length; i++) {
+        const char = password[i];
+        if (char >= '0' && char <= '9') {
+            hasDigit = true;
+            break;
+        }
+    }
+
+    return hasMinLength && hasDigit;
 }
+
+
 
 module.exports = {validatePassword}
--- passwordValidator.spec.js
+++ passwordValidator.spec.js
@@ -2,15 +2,24 @@ const {validatePassword} = require('./passwordValidator');
 
 describe('Password Validator', () => {
 
-      test('Valid : the password has 8 or more characters', () => {
-        expect(validatePassword('aaaabbbbcccc')).toBe(true);
-      })
+    test('Valid : the password has 8 or more characters', () => {
+        expect(validatePassword('1aaabbbbcccc')).toBe(true);
+    })
 
-      test('Valid : the password has exactly 8 characters', () => {
-        expect(validatePassword('aaaabbbb')).toBe(true);
-      })
+    test('Valid : the password has exactly 8 characters', () => {
+        expect(validatePassword('1aaabbbb')).toBe(true);
+    })
 
-      test('Invalid : the password has less than 8 characters', () => {
-        expect(validatePassword('aaaa')).toBe(false);
-      })
+    test('Invalid : the password has less than 8 characters', () => {
+        expect(validatePassword('1aaa')).toBe(false);
+    })
+
+    test('valid : the password has digits', () => {
+        expect(validatePassword('1aaabbbb')).toBe(true);
+    })
+
+    test('invalid : the password has no digits', () => {
+        expect(validatePassword('aaaabbbb')).toBe(false);
+    })
+
 })
